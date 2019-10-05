c1d308bd41775c2478441a2d533f8279a5b2f12b
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2019-10-04 20:13:29 -0400

    fix no results page

--- Arc9_Frontend/src/components/config.js
+++ Arc9_Frontend/src/components/config.js
@@ -1,6 +1,11 @@
 export default {
-     api_key: "9506ba1962e5dd0dcd0e2a677905d8d2"
+    //  api_key: "9506ba1962e5dd0dcd0e2a677905d8d2"
+    // api_key: "145aab907019b163db8eeb61eebad053"
+    api_key: "5f95e5ffbda92c3d8f563b37ef402197"
+
+
 }
 
+// 5f95e5ffbda92c3d8f563b37ef402197
 
 //api key is limit to 10,000 calls
