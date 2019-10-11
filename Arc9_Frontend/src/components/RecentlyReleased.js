302c583ce5ffb427c45902bd449ccc7a2195c9be
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2019-10-10 23:24:16 -0400

    last carousels to be added to the homepage

---
+++ Arc9_Frontend/src/components/RecentlyReleased.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import config from './config';
+import './Games.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {Link} from 'react-router-dom';
+
+
+// console.log(config);
+// config.api_key
+
+
+class RecentlyReleased extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            Recently: []
+        }
+    }
+    componentDidMount(){
+
+        var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+        const API_KEY = `${config.api_key}`;
+        axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+        //use the timestamp to find the most recent relases games...
+        var timestamp = Math.round(new Date().getTime()/1000);
+       
+        axios({
+        url: proxyUrl + "https://api-v3.igdb.com/games",
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'user-key': API_KEY
+        },
+        data: `fields age_ratings.rating,aggregated_rating,aggregated_rating_count,alternative_names.name,artworks.url,bundles,category,collection,cover.height,cover.image_id,cover.url,cover.width,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise.name,franchises.name,game_engines,game_modes.name,genres.name,hypes,involved_companies,keywords.name,multiplayer_modes,name,parent_game,platforms.name,player_perspectives.name,popularity,pulse_count,rating,rating_count,release_dates.date,release_dates.human,screenshots.image_id,screenshots.height,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes.name,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos.video_id,websites.url; where platforms = (3,9,11,12,48) & platforms != (6) & cover !=n & genres !=n & release_dates !=n & first_release_date < ${timestamp}; sort first_release_date desc; limit 50;`
+        
+    })
+        .then(response => {
+            const gameData = response.data;
+            // console.log(gameData);
+            // console.log(response.data);
+            this.setState({ 
+                Recently: gameData
+
+            })
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+
+    }
+
+
+
+    render(){
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+           
+          };
+       
+       
+        const gameCoverUrl = "https://images.igdb.com/igdb/image/upload/t_cover_big_2x/"
+        const recentlyCameOut = this.state.Recently.map((game,index)=>{
+            return(
+                <div className="row s3"  key={index}>
+                    <Link to={`/games/${game.id}`}>
+                        <img id= "ggg" src={`${gameCoverUrl}${game.cover.image_id}.jpg`} height="90%" width="90%" alt="gameImage" />
+                    </Link>
+                        <span className="gametitle">{game.name}</span> <br></br>
+                        <span className="dataRelease">Date Release: {game.release_dates[0].human}</span>
+
+                      
+                    </div>
+
+                    
+
+            )
+        })
+    return(
+        // <div> {gameList}</div>
+        <div>
+        <h2> Recently Released  </h2>
+        <Slider {...settings}>
+             {recentlyCameOut}
+            
+        </Slider>
+      </div>
+
+    )
+    }
+}
+
+
+
+
+export default RecentlyReleased;
